c625e21bf3450e76676c41a431d639b82f1afdbd
WFT <will.a.ft@gmail.com>
2014-03-25 11:06:44 -0400

    resolving some void* issues

--- stack.c
+++ stack.c
@@ -19,7 +19,7 @@ void *popstack(struct stack *s) {
 // pushes the value pointed to by val onto the stack s, returns new length
 int pushstack(struct stack *s, void *val) {
   if (s->index == (s->size / s->isize) - 1)
-    realloc(ret->data, s->size * 2);
+    s->data = realloc(s->data, s->size * 2);
   s->size *= 2;
   s->index += 1;
   s->data[s->index * s->isize] = *val;
@@ -28,10 +28,10 @@ int pushstack(struct stack *s, void *val) {
 
 // constructs a stack with space for numitems items of isize bytes per item
 struct stack *constructstack(int isize, int numitems) {
-  Stack ret;
-  ret.data = malloc(isize * numitems);
-  ret.isize = isize;
-  ret.index = -1;
-  ret.size = isize * numitems;
-  return &ret;
+  Stack *ret = malloc(sizeof(Stack));
+  ret->data = malloc(isize * numitems);
+  ret->isize = isize;
+  ret->index = -1;
+  ret->size = isize * numitems;
+  return ret;
 }
