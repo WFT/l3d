218a95664f6c6e6d60c9f893224e7d6a922c1d3f
WFT <will.a.ft@gmail.com>
2014-05-17 15:20:36 -0400

    nearly working triangle drawing

--- lines.c
+++ lines.c
@@ -1,4 +1,76 @@
 #include "lines.h"
+#include "display.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+BLine *ab;
+BLine *bc;
+BLine *cd;
+BLine *da;
+
+
+void bline_load(BLine * line) {
+  order_endpoints(line);
+  int count = point_count(line);
+  size_t b = count * sizeof(int);
+  realloc(line->x_points, b);
+  realloc(line->y_points, b);
+  realloc(line->z_points, count * sizeof(double));
+  find_points(line);
+}
+
+void draw_triangle(int coors[9], uint32_t color) {
+  ab->x1 = coors[0];
+  ab->y1 = coors[1];
+  ab->z1 = coors[2];
+  ab->x2 = coors[3];
+  ab->y2 = coors[4];
+  ab->z2 = coors[5];
+
+  bc->x1 = coors[3];
+  bc->y1 = coors[4];
+  bc->z1 = coors[5];
+  bc->x2 = coors[6];
+  bc->y2 = coors[7];
+  bc->z2 = coors[8];
+
+  cd->x1 = coors[6];
+  cd->y1 = coors[7];
+  cd->z1 = coors[8];
+  cd->x2 = coors[0];
+  cd->y2 = coors[1];
+  cd->z2 = coors[2];
+
+
+  // find the mid y value
+  int max_y = coors[1] > coors[4] ? coors[1]:coors[4];
+  max_y = coors[7] > max_y ? coors[7]:max_y;
+  int min_y = coors[1] < coors[4] ? coors[1]:coors[4];
+  min_y = coors[7] < min_y ? coors[7]:min_y;
+  int mid_y;
+  if (coors[1] != max_y && coors[1] != min_y)
+    mid_y = coors[1];
+  else if (coors[4] != max_y && coors[4] != min_y)
+    mid_y = coors[4];
+  else if (coors[7] != max_y && coors[7] != min_y)
+    mid_y = coors[7];
+
+  bline_load(ab);
+  bline_load(bc);
+  bline_load(cd);
+
+  int p, count = point_count(ab);
+  for (p = 0; p < count; p++)
+    setpix(ab->x_points[p], ab->y_points[p], color, 0);
+
+  count = point_count(bc);
+  for (p = 0; p < count; p++)
+    setpix(bc->x_points[p], bc->y_points[p], color, 0);
+
+  count = point_count(cd);
+  for (p = 0; p < count; p++)
+    setpix(ab->x_points[p], ab->y_points[p], color, 0);
+}
 
 void order_endpoints(BLine *line) {
   if (line->x1 > line->x2) {
@@ -14,14 +86,60 @@ void order_endpoints(BLine *line) {
   }
 }
 
-void draw_cross(BLine *l1, BLine *l2) {
-  
-}
-
 void find_points(BLine *line) {
-  
+  int dx = line->x2 - line->x1;
+  int dy = line->y2 > line->y1?line->y2 - line->y1:line->y1 - line->y2;
+  int x = line->x1, y = line->y1;
+  // y goes up if y1 is smaller than y2, else it goes down
+  int ystep = line->y1 < line->y2 ? 1 : -1;
+  int p = 0;
+  if (dx > dy) {
+    int acc  = dx/2;
+    while (x < line->x2) {
+      line->x_points[p] = x;
+      line->y_points[p] = y;
+      bresenham_step(&acc, &x, &y, dx, dy, 1, ystep);
+      p++;
+    }
+  } else {
+    int  acc = dy/2;
+    char up = line->y1 < line->y2;
+    while (up ? y <= line->y2 : y >= line->y2) {
+      line->x_points[p] = x;
+      line->y_points[p] = y;
+      bresenham_step(&acc, &y, &x, dy, dx, ystep, 1);
+      p++;
+    }
+  }
+  // creates a rough approximation of evenly spaced z coordinates
+  p = 0;
+  double z = line->z1;
+  double zstep = (line->z1 - line->z2) / point_count(line);
+  while (z < line->z2) {
+    line->z_points[p] = z;
+    z += zstep;
+  }
 }
 
-void advance_line(BLine *line) {
-  
+void bresenham_step(int *acc, int *major_counter, int *minor_counter, int major_delta, int minor_delta, int major_step, int minor_step) {
+  *acc -= minor_delta;
+  if (*acc < 0) {
+    *minor_counter += minor_step;
+    *acc += major_delta;
+  }
+  *major_counter += major_step;
+}
+
+void draw_horizontals(int x1, int x2, int y, uint32_t color) {
+  int x = x1;
+  while (x <= x2) {
+    setpix(x, y, color, 0);
+    x++;
+  }
+}
+
+int point_count(BLine *line)  {
+  int dx = line->x2 - line->x1;
+  int dy = line->y2 - line->y1;
+  return dx > dy ? dx + 1 : dy + 1;
 }
--- lines.h
+++ lines.h
@@ -1,19 +1,29 @@
+#pragma once
+#include <inttypes.h>
+
 typedef struct bresenham_line {
   int x1, y1, z1, x2, y2, z2;
-  int acc_xy, acc_xz;
   int *x_points;
   int *y_points;
-  int *z_points;
+  double *z_points;
 } BLine;
+
+#define BLINE_DEFAULT = ((BLine){-1, -1, -1, -1, -1, -1, NULL, NULL, NULL});
+
+// sets up the BLine using the endpoints already set
+void bline_load(BLine *line);
+
+// takes an array of six coordinates alternating x y z
+void draw_triangle(int coors[9], uint32_t color);
 
 // order points by x
 void order_endpoints(BLine *line);
 
-// draw across
-void draw_cross(BLine *l1, BLine *l2);
-
-// discover all points from xyz1 to xyz2
+// discover all points using the bresenham_step
 void find_points(BLine *line);
 
-// bresenham advance
-void advance_line(BLine *line);
+// generalized bresenham line algorithm will advance one step for each call
+void bresenham_step(int *acc, int *major_counter, int *minor_counter, int major_delta, int minor_delta, int major_step, int minor_step);
+
+// predicts how many points there will be
+int point_count(BLine *line);
