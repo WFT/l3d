a8f495099e07dd0c92a944cf924d82aac7327872
WFT <will.a.ft@gmail.com>
2014-05-25 15:38:45 -0400

    color/constant change

--- src/options.h
+++ src/options.h
@@ -5,8 +5,8 @@
 
 // Line drawing:
 
-#define DRAW_HORIZONTALS 1
+#define DRAW_FILL 1
 #define DRAW_VERTICES 1
-#define VERTICES_COLOR 0xFF0000
+#define VERTICES_COLOR 0xFFFF00
 #define DRAW_EDGES 1
-#define EDGE_COLOR 0x0000FF
+#define EDGE_COLOR 0xFF00FF
