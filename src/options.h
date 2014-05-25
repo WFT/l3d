093c970e96a6fd0df7895f9140d3f8d8bfab337c
WFT <will.a.ft@gmail.com>
2014-05-25 16:17:15 -0400

    spinclolor

--- src/options.h
+++ src/options.h
@@ -5,8 +5,12 @@
 
 // Line drawing:
 
+#define SPIN_FILL_COLOR 0xFFFF00
+
 #define DRAW_FILL 1
+
 #define DRAW_VERTICES 1
-#define VERTICES_COLOR 0xFFFF00
+#define VERTICES_COLOR 0xFF00FF
+
 #define DRAW_EDGES 1
-#define EDGE_COLOR 0xFF00FF
+#define EDGE_COLOR 0x0000FF
