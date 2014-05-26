6f9832b6bf929075df2cc5717fe22532dba5e70f
WFT <will.a.ft@gmail.com>
2014-05-25 20:46:20 -0400

    for reference

--- src/lines.c
+++ src/lines.c
@@ -302,7 +302,10 @@ void draw_triangle(int coors[6], uint32_t color) {
     }
   }
   shorti = 0;
-
+  /*do
+    shorti += lower_inc;
+  while (shorti * lower_inc < lower_count
+  && lower_segment_y[shorti + lower_inc] == lower_segment_y[shorti]);*/
   while (shorti * lower_inc < lower_count && longi * long_inc < long_count) {
     draw_horizontal(lower_segment_x[shorti], long_segment_x[longi],
 		    lower_segment_y[shorti], color);
