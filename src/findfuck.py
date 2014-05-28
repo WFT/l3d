aa2a0ba9f3ec52094a830a8e0ed231ce9861c69f
WFT <will.a.ft@gmail.com>
2014-05-27 20:30:00 -0400

    fucks found

--- src/findfuck.py
+++ src/findfuck.py
@@ -15,14 +15,13 @@ if len(sys.argv) > 1:
             elif l[0:2] != 'x1':
                 continue
             newy = int(l.split(':')[-1])
-            if y == -1:
-                y = newy
-            else:
-                if newy - 1 != y:
+            if y >= 0:
+                if newy != y - 1:
                     if y == newy:
                         print 'eq',
                     else:
                         print 'nequals',
-                    print 'found on line ', i - 1
+                    print 'found on line %d (%d -> %d)'%(i - 1, y, newy)
+            y = newy
 
 print 'done;'
