711e3dc56234323f06234462b86fd0f464c40d85
WFT <will.a.ft@gmail.com>
2014-05-27 20:19:19 -0400

    script for finding errors in filled lines

---
+++ src/findfuck.py
@@ -0,0 +1,30 @@
+import sys
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1]) as f:
+        i = 1
+        y = -1
+        for l in f:
+            i += 1
+            l = l.strip()
+            if l == 'MAN DOWN':
+                break
+            elif l == 'drew triangle':
+                y = -1
+                continue
+            elif l in 'arrived':
+                continue
+            elif l in 'skipping':
+                continue
+            newy = int(l.split(':')[-1])
+            if y == -1:
+                y = newy
+            else:
+                if newy != y - 1:
+                    if y == newy:
+                        print 'eq',
+                    else:
+                        print 'nequals',
+                    print 'found on line ', i
+
+print 'done;'
