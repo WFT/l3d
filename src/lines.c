369821a8e8ed4a8ee081b614c901464b8d5b3195
WFT <will.a.ft@gmail.com>
2014-06-03 10:59:15 -0400

    beginning refactor (end of class)

--- src/lines.c
+++ src/lines.c
@@ -4,15 +4,11 @@
 #include "display.h"
 #include "options.h"
 
-// order points by x
-inline void order_endpoints(int *x1, int *y1, int *x2, int *y2) {
-  if (*x1 > *x2) {
-    int swap = *x1;
-    *x1 = *x2;
-    *x2 = swap;
-    swap = *y1;
-    *y1 = *y2;
-    *y2 = swap;
+inline void order_endpoints(KZ_Point *p1, KZ_Point *p2) {
+  if (p2->x > p1->x) {
+    KZ_Point swap = *p1;
+    *p1 = *p2;
+    *p2 = swap;
   }
 }
 
@@ -26,9 +22,12 @@ inline void bresenham_step(int *acc, int *major_counter, int *minor_counter, int
   *major_counter += major_step;
 }
 
-// points should be ordered first
 // predicts how many points there will be
-inline int point_count(int x1, int y1, int x2, int y2)  {
+inline int point_count(KZ_Point p1, KZ_Point p2)  {
+  KZ_Point greatP = p2;
+  KZ_Point littleP = p1;
+  order_endpoints(*littleP, *greatP);
+
   int dx = x2 - x1;
   int dy = y2 - y1;
   if (dy < 0)
@@ -61,17 +60,20 @@ inline void draw_horizontal(int x1, int x2, int y, uint32_t color) {
 
 // discover all points using the bresenham_step
 // RETURNS: number of points actually found
-inline int find_points(int x1, int y1, int x2, int y2,
-                       int *x_points, int *y_points) {
-  int dx = x2 - x1;
-  int dy = y2 > y1?y2 - y1:y1 - y2;
-  int x = x1, y = y1;
-  // y goes up if y1 is smaller than y2, else it goes down
-  int ystep = y1 < y2 ? 1 : -1;
+inline int find_points(KZ_Point p1, KZ_Point p2, int *points) {
+  KZ_Point greatP = p2;
+  KZ_Point littleP = p1;
+  order_endpoints(*littleP, *greatP);
+
+  int dx = greatP.x - p1.x;
+  int dy = greatP.y > littlP.y?greatP.y - littlP.y:littlP.y - greatP.y;
+  int x = p1.x, y = littlP.y;
+  // y goes up if littlP.y is smaller than greatP.y, else it goes down
+  int ystep = littlP.y < greatP.y ? 1 : -1;
   int p = 0;
   if (dx > dy) {
     int acc  = dx/2;
-    while (x <= x2) {
+    while (x <= greatP.x) {
       x_points[p] = x;
       y_points[p] = y;
       bresenham_step(&acc, &x, &y, dx, dy, 1, ystep);
@@ -79,8 +81,8 @@ inline int find_points(int x1, int y1, int x2, int y2,
     }
   } else {
     int  acc = dy/2;
-    char up = y1 < y2;
-    while (up ? y <= y2 : y >= y2) {
+    char up = littlP.y < greatP.y;
+    while (up ? y <= greatP.y : y >= greatP.y) {
       x_points[p] = x;
       y_points[p] = y;
       bresenham_step(&acc, &y, &x, dy, dx, ystep, 1);
@@ -93,103 +95,66 @@ inline int find_points(int x1, int y1, int x2, int y2,
 // takes an array of six coordinates alternating x y z
 void draw_triangle(KZ_Point a, KZ_Point b, KZ_Point c,
 		   uint32_t color) {
-  int ax = a.x;
-  int ay = a.y;
-  int bx = b.x;
-  int by = b.y;
-  int cx = c.x;
-  int cy = c.y;
 
   lock_surface();
+  
+  
+  int ab_count = point_count(a, b);
+  KZ_Point *ab_points = malloc((ab_count + 1) * sizeof(KZ_Point));
+  ab_count = find_points(a, b, ab_points);
 
-  order_endpoints(&ax, &ay, &bx, &by);
-  int ab_count = point_count(ax, ay, bx, by);
-  int *ab_x_points = malloc((ab_count + 1) * sizeof(int));
-  int *ab_y_points = malloc((ab_count + 1) * sizeof(int));
-  ab_count = find_points(ax, ay, bx, by,
-                         ab_x_points, ab_y_points);
+  int bc_count = point_count(b, c);
+  *bc_points = malloc((bc_count + 1) * sizeof(KZ_Point));
+  bc_count = find_points(b, c, bc_points);
 
-
-  bx = b.x;
-  by = b.y;
-  order_endpoints(&bx, &by, &cx, &cy);
-  int bc_count = point_count(bx, by, cx, cy);
-  int *bc_x_points = malloc((bc_count + 1) * sizeof(int));
-  int *bc_y_points = malloc((bc_count + 1) * sizeof(int));
-  bc_count = find_points(bx, by,  cx, cy,
-                         bc_x_points, bc_y_points);
-
-  ax = a.x;
-  ay = a.y;
-  cx = c.x;
-  cy = c.y;
-  order_endpoints(&cx, &cy, &ax, &ay);
-  int ca_count = point_count(cx, cy, ax, ay);
-  int *ca_x_points = malloc((ca_count + 1) * sizeof(int));
-  int *ca_y_points = malloc((ca_count + 1) * sizeof(int));
-  ca_count = find_points(cx, cy, ax, ay,
-                         ca_x_points, ca_y_points);
+  int ca_count = point_count(c, a);
+  *ca_points = malloc((ca_count + 1) * sizeof(KZ_Point));
+  ca_count = find_points(c, a, ca_points);
 
 #if DRAW_FILL
 
   // order arrays by y descending
 
-  int *abxs;
-  int *abys;
+  int *abs;
   int abinc;
   if (ab_y_points[ab_count - 1] > ab_y_points[0]) {
-    abxs = ab_x_points + ab_count - 1;
-    abys = ab_y_points + ab_count - 1;
+    abs = ab_points + ab_count - 1;
     abinc = -1;
   } else {
-    abxs = ab_x_points;
-    abys = ab_y_points;
+    abs = ab_points;
     abinc = 1;
   }
 
-  int *bcxs;
-  int *bcys;
+  int *bcs;
   int bcinc;
   if (bc_y_points[bc_count  - 1] > bc_y_points[0]) {
-    bcxs = bc_x_points + bc_count - 1;
-    bcys = bc_y_points + bc_count - 1;
+    bcs = bc_points + bc_count - 1;
     bcinc = -1;
   } else {
-    bcxs = bc_x_points;
-    bcys = bc_y_points;
+    bcs = bc_points;
     bcinc = 1;
   }
 
-  int *caxs;
-  int *cays;
+  int *cas;
   int cainc;
   if (ca_y_points[ca_count  - 1] > ca_y_points[0]) {
-    caxs = ca_x_points + ca_count - 1;
-    cays = ca_y_points + ca_count - 1;
+    cas = ca_points + ca_count - 1;
     cainc = -1;
   } else {
-    caxs = ca_x_points;
-    cays = ca_y_points;
+    cas = ca_points;
     cainc = 1;
   }
 
-  ax = a.x;
-  ay = a.y;
-  bx = b.x;
-  by = b.y;
-  cx = c.x;
-  cy = c.y;
-
   // find the mid y value
-  int max_y = ay > by ? ay:by;
-  max_y = cy > max_y ? cy:max_y;
+  int max_y = a.y > b.y ? a.y:b.y;
+  max_y = c.y > max_y ? c.y:max_y;
   int mid_y;
-  if (max_y == ay) {
-    mid_y = by > cy ? by : cy;
-  } else if (max_y == by) {
-    mid_y = ay > cy ? ay : cy;
-  } else if (max_y == cy) {
-    mid_y = by > ay ? by : ay;
+  if (max_y == a.y) {
+    mid_y = b.y > c.y ? b.y : c.y;
+  } else if (max_y == b.y) {
+    mid_y = a.y > c.y ? a.y : c.y;
+  } else if (max_y == c.y) {
+    mid_y = b.y > a.y ? b.y : a.y;
   } else {
     printf("mid_y can't be found\n");
     return;
@@ -197,95 +162,77 @@ void draw_triangle(KZ_Point a, KZ_Point b, KZ_Point c,
 
   //categorize lines by upper, lower, long
 
-  int *upper_segment_x = NULL;
-  int *upper_segment_y = NULL;
+  int *upper_segment = NULL;
   int upper_inc = 0;
   int upper_count = 0;
-  int *lower_segment_x = NULL;
-  int *lower_segment_y = NULL;
+  int *lower_segment = NULL;
   int lower_inc = 0;
   int lower_count = 0;
-  int *long_segment_x = NULL;
-  int *long_segment_y = NULL;
+  int *long_segment = NULL;
   int long_inc = 0;
   int long_count = 0;
 
   if (mid_y == ay) {
     long_inc = bcinc;
-    long_segment_x = bcxs;
-    long_segment_y = bcys;
+    long_segment = bcs;
     long_count = bc_count;
     if (max_y == by) {
       upper_inc = abinc;
-      upper_segment_x = abxs;
-      upper_segment_y = abys;
+      upper_segment = abs;
       upper_count = ab_count;
 
       lower_inc = cainc;
-      lower_segment_x = caxs;
-      lower_segment_y = cays;
+      lower_segment = cas;
       lower_count = ca_count;
     } else if (max_y == cy) {
       upper_inc = cainc;
-      upper_segment_x = caxs;
-      upper_segment_y = cays;
+      upper_segment = cas;
       upper_count = ca_count;
 
       lower_inc = abinc;
-      lower_segment_x = abxs;
-      lower_segment_y = abys;
+      lower_segment = abs;
       lower_count = ab_count;
     }
   } else if (mid_y == by) {
     long_inc = cainc;
-    long_segment_x = caxs;
-    long_segment_y = cays;
+    long_segment = cas;
     long_count = ca_count;
     if (max_y == ay) {
       upper_inc = abinc;
-      upper_segment_x = abxs;
-      upper_segment_y = abys;
+      upper_segment = abs;
       upper_count = ab_count;
 
       lower_inc = bcinc;
-      lower_segment_x = bcxs;
-      lower_segment_y = bcys;
+      lower_segment = bcs;
       lower_count = bc_count;
     } else if (max_y == cy) {
       upper_inc = bcinc;
-      upper_segment_x = bcxs;
-      upper_segment_y = bcys;
+      upper_segment = bcs;
       upper_count = bc_count;
 
       lower_inc = abinc;
-      lower_segment_x = abxs;
-      lower_segment_y = abys;
+      lower_segment = abs;
       lower_count = ab_count;
     }
   } else if (mid_y == cy) {
     long_inc = abinc;
-    long_segment_x = abxs;
-    long_segment_y = abys;
+    long_segment = abs;
     long_count = ab_count;
     if (max_y == ay) {
       upper_inc = cainc;
-      upper_segment_x = caxs;
-      upper_segment_y = cays;
+      upper_segment = cas;
       upper_count = ca_count;
 
       lower_inc = bcinc;
-      lower_segment_x = bcxs;
-      lower_segment_y = bcys;
+      lower_segment = bcs;
       lower_count = bc_count;
     } else if (max_y == by) {
       upper_inc = bcinc;
-      upper_segment_x = bcxs;
-      upper_segment_y = bcys;
+      upper_segment = bcs;
       upper_count = bc_count;
 
       lower_inc = cainc;
-      lower_segment_x = caxs;
-      lower_segment_y = cays;
+      lower_segment = cas;
       lower_count = ca_count;
     }
   }
