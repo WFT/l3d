aff305e81a7d5c6e84d6e09e20e5bc536e51bfa7
WFT <will.a.ft@gmail.com>
2014-06-06 10:50:30 -0400

    commenting spin stereo for now

--- src/render.h
+++ src/render.h
@@ -7,7 +7,6 @@ void renderperspective(Matrix *faces, double *eye, uint32_t color);
 
 // rendering functions for screen
 void rendercyclops(Matrix *faces, double *eye);
-void renderstereo(Matrix *faces, double *eyes);
-void renderparallel(Matrix *faces);
+//void renderstereo(Matrix *faces, double *eyes);
 void spincyclops(Matrix *edge, double *eye, int del);
-void spinstereo(Matrix *edge, double *eyes, int del);
+//void spinstereo(Matrix *edge, double *eyes, int del);
